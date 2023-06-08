774fc6981bb88d0705c5e9642b3465b7b8bc0ad4
Guillermo Riaño <guilledark17@gmail.com>
2023-06-08 11:50:08 -0600

    ✅ Add Entity Classes

--- ASP.NET_EntityLayer/Department.cs
+++ ASP.NET_EntityLayer/Department.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace ASP.NET_EntityLayer
 {
-    public class Class1
+    public class Department
     {
+        public int departmentId {  get; set; }
+        public string departmentName { get; set; }
     }
 }
---
+++ ASP.NET_EntityLayer/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_EntityLayer
+{
+    public class Employee
+    {
+        public int employeeId { get; set; }
+        public string employeeName { get; set; }
+        public Department department { get; set; }
+        public decimal salary { get; set; }
+        public string contractDate { get; set; }
+    }
+}
