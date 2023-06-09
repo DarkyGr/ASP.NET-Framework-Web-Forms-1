0a56c464c1b2e3c1c1b33a9bd93bc9da2461333a
Guillermo Riaño <guilledark17@gmail.com>
2023-06-09 12:24:36 -0600

    ✅ Adding Department DataLayer

---
+++ ASP.NET_DataLayer/DepartmentDL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASP.NET_EntityLayer;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASP.NET_DataLayer
+{
+    public class DepartmentDL
+    {
+        public List<Department> listDepartment() {
+            List<Department> list = new List<Department>();
+
+            // Create connection
+            using (SqlConnection oConnection = new SqlConnection(Connection.SQLString))
+            {
+                // Create query with the function created
+                SqlCommand cmd = new SqlCommand("select * from fn_departments()", oConnection);
+                cmd.CommandType = CommandType.Text;
+
+                try
+                {
+                    oConnection.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            list.Add(new Department
+                            {
+                                departmentId = Convert.ToInt32(dr["departmentId"].ToString()),
+                                departmentName = dr["departmentName"].ToString()
+                            });
+                        }
+                    }
+
+                    return list;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }            
+        }
+    }
+}
