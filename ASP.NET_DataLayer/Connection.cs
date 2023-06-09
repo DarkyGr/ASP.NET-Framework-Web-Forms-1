9c1f914d7205484a34cc2e652c673a39f9c5fa15
Guillermo Riaño <guilledark17@gmail.com>
2023-06-09 11:59:13 -0600

    ✅ Adding Connection to Database

---
+++ ASP.NET_DataLayer/Connection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NET_DataLayer
+{
+    public class Connection
+    {
+        // Connection from WebForms Layer
+        public static string SQLString = ConfigurationManager.ConnectionStrings["SQLString"].ToString();
+    }
+}
