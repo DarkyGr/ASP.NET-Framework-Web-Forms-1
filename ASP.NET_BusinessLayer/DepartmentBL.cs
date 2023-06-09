7590fbd6f31a607973650c2649d11fd363967e5c
Guillermo Riaño <guilledark17@gmail.com>
2023-06-09 12:56:06 -0600

    ✅ Adding Department BusinessLayer

---
+++ ASP.NET_BusinessLayer/DepartmentBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASP.NET_DataLayer;
+using ASP.NET_EntityLayer;
+
+namespace ASP.NET_BusinessLayer
+{
+    public class DepartmentBL
+    {
+        DepartmentDL departmentDL = new DepartmentDL();
+
+        public List<Department> list()
+        {
+            try
+            {
+                return departmentDL.listDepartment();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
