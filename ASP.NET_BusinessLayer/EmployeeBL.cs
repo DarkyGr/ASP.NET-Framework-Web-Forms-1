6480cc05c19cf4ec39fe908f6c78c84c3542fcf0
Guillermo Riaño <guilledark17@gmail.com>
2023-06-09 12:56:14 -0600

    ✅ Adding Employee BusinessLayer

---
+++ ASP.NET_BusinessLayer/EmployeeBL.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ASP.NET_DataLayer;
+using ASP.NET_EntityLayer;
+
+namespace ASP.NET_BusinessLayer
+{
+    public class EmployeeBL
+    {
+        EmployeeDL employeeDL = new EmployeeDL();
+
+        public List<Employee> list()
+        {
+            try
+            {
+                return employeeDL.listEmployee();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Employee getEmployee(int employeeId) {
+            try
+            {
+                return employeeDL.getEmployee(employeeId);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool createEmployee(Employee entity)
+        {
+            try
+            {
+                if (entity.employeeName == "")
+                {
+                    throw new OperationCanceledException("The name field cannot be empty");
+                }
+
+                return employeeDL.createEmployee(entity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool updateEmployee(Employee entity)
+        {
+            try
+            {
+                var found = employeeDL.getEmployee(entity.employeeId);
+
+                if (found.employeeId == 0)
+                {
+                    throw new OperationCanceledException("The employee doesn't exist");
+                }
+
+                return employeeDL.updateEmployee(entity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool deleteEmployee(int employeeId)
+        {
+            try
+            {
+                var found = employeeDL.getEmployee(employeeId);
+
+                if (found.employeeId == 0)
+                {
+                    throw new OperationCanceledException("The employee doesn't exist");
+                }
+
+                return employeeDL.deleteEmployee(employeeId);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
